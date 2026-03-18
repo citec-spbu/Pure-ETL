454217f0491906f581be94fd9c93e9bd9d9a8e25
forgblord <radkova.irene@gmail.com>
2026-03-18 22:04:33 +0300

    refactor: remove redundant set variable when finding top level key types

--- experiments/explore-persons-organisations/notebooks/json_utils.py
+++ experiments/explore-persons-organisations/notebooks/json_utils.py
@@ -9,16 +9,13 @@ def load_from_json(path: str) -> list:
 def find_top_level_keys(items: list) -> dict:
     # Find all top-level keys with their types
     
-    top_level_keys = set()
     top_level_keys_typed = dict()
 
     for item in items:
         for key in item.keys():
-            if key not in top_level_keys:
-                top_level_keys.add(key)
-                if key not in top_level_keys_typed:
-                    top_level_keys_typed[key] = set()
-                top_level_keys_typed[key].add(type(item[key]))
+            if key not in top_level_keys_typed:
+                top_level_keys_typed[key] = set()
+            top_level_keys_typed[key].add(type(item[key]))
     
     return top_level_keys_typed
 
